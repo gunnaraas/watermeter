bf956af43bc44986af6de9a0ed84d77c1e1004e9
gunnaraas <gunnar.aas218@gmail.com>
2023-07-16 17:33:07 -0500

    Create watermeter.sh

---
+++ watermeter.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# requires that the following go programs are installed in ~/go/bin
+# - go install github.com/abaker/stdout_httpd@latest
+# - go install github.com/bemasher/rtlamr@latest
+
+# requires that rtl_tcp is installed on Raspberry Pi, use these instructions:
+# https://gist.github.com/floehopper/99a0c8931f9d779b0998
+
+rtl_tcp & sleep 5
+./go/bin/rtlamr -msgtype=r900 -centerfreq=912000000 -filterid=1831543680 -format=json |
+  jq --unbuffered -c '. |= del(.Message) + .Message' |
+  ./go/bin/stdout_httpd -port 8080
